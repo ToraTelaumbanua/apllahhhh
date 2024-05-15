c37462a38f2047564ac96699f91d7712dcf6539a
fransiskus23 <fransiskus.oktanesius.l@mail.ukrim.ac.id>
2024-05-16 04:21:48 +0700

    fiksBaru

--- database/migrations/2024_04_19_011628_create_product_table.php
+++ database/migrations/2024_04_19_011628_create_product_table.php
@@ -7,25 +7,32 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->id();
-            $table->string('barcode');
-            $table->string('name');
-            $table->decimal('price', 20, 0);
-            $table->softDeletes();
+            $table->id('id_produk');
+            $table->string('Product_Name');
+            $table->decimal('Price', 10, 2);
+            $table->text('Description')->nullable();
+            $table->string('image')->nullable();
+            $table->integer('id_kategori');
+            $table->foreign('id_kategori')->references('id_kategori')->on('kategori');
             $table->timestamps();
+            $table->softDeletes(); // Tambahkan softDeletes untuk menggunakan soft delete
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('products');
     }
-};
 
+};
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,8 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\Product;
-use Faker\Factory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class ProductSeeder extends Seeder
@@ -13,43 +12,6 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        $faker = Factory::create('id_ID');
-        #product1
-        $p = new Product();
-        $p->barcode = $faker->numerify('######');
-        $p->name = 'LCD Epson';
-        $p->price = 4500000;
-        $p->save();
-        #product2
-        $p = new Product();
-        $p->barcode = $faker->numerify('######');
-        $p->name = 'Permen Kopiko';
-        $p->price = 2000;
-        $p->save();
-        #product3
-        $p = new Product();
-        $p->barcode = $faker->numerify('######');
-        $p->name = 'Ac Sharp Inverter 2PK';
-        $p->price = 7500000;
-        $p->save();
-        #product4
-        $p = new Product();
-        $p->barcode = $faker->numerify('######');
-        $p->name = 'Bunga Plastik Palsu';
-        $p->price = 25000;
-        $p->save();
-        #product5
-        $p = new Product();
-        $p->barcode = $faker->numerify('######');
-        $p->name = 'Spidol Snowman';
-        $p->price = 15000;
-        $p->save();
-        #product6
-        $p = new Product();
-        $p->barcode = $faker->numerify('######');
-        $p->name = 'Janda';
-        $p->price = 350000;
-        $p->save();
-        #php artisan db:seed --class=ProductSeeder
+        //
     }
 }
